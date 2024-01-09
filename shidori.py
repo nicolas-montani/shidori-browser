2f1aaa3eca7528e57a3c274b8c08e97b8037a0c8
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2024-01-09 22:26:21 +0100

    ui creation

--- shidori.py
+++ shidori.py
@@ -1,14 +1,17 @@
+#!/usr/bin/python3.11
+
 import socket
 import ssl
 import tkinter
 
-class URL: 
+
+class URL:
     def __init__(self, url):
-        self.scheme, url = url.split('://',1)
+        self.scheme, url = url.split('://', 1)
         assert self.scheme in ['http', 'https']
         if "/" not in url:
-            url = url + "/" 
-        self.host, url = url.split("/",1)
+            url = url + "/"
+        self.host, url = url.split("/", 1)
         self.path = "/" + url
         if self.scheme == "http":
             self.port = 80
@@ -17,6 +20,8 @@ class URL:
         if ":" in self.host:
             self.host, port = self.host.split(":", 1)
             self.port = int(port)
+        
+
 
     def request(self):
         s = socket.socket(
@@ -25,24 +30,25 @@ class URL:
             proto=socket.IPPROTO_TCP,
         )
         s.connect((self.host, self.port))
-        if self.scheme == "https": 
+        if self.scheme == "https":
             ctx = ssl._create_unverified_context()
 
             s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        s.send(("GET {} HTTP/1.0\r\n".format(self.path) + \
-                "Host: {}\r\n\r\n".format(self.host)) \
+        s.send(("GET {} HTTP/1.0\r\n".format(self.path) +
+                "Host: {}\r\n\r\n".format(self.host))
                .encode("utf8"))
-        
+
         response = s.makefile("r", encoding=("utf8"), newline="\r\n")
         statusline = response.readline()
         version, status, explanation = statusline.split(" ", 2)
-        response_headers={}
+        response_headers = {}
 
         while True:
             line = response.readline()
-            if line == "\r\n":break
-            header, value = line.split(":",1)
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
 
         assert "transfer-encoding" not in response_headers
@@ -51,43 +57,72 @@ class URL:
         s.close()
         return body
     
+def lex(body):
+    text = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            text += c
+    return text
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13,18
+SCROLL_STEP = 100
+
+
+def layout(text):
+        display_list = []
+        cursor_x, cursor_y = HSTEP, VSTEP   
+        for c in text:
+            display_list.append((cursor_x, cursor_y,c))
+            cursor_x += HSTEP
+            if cursor_x >= WIDTH - HSTEP:
+                cursor_y += VSTEP
+                cursor_x = HSTEP
+        return display_list
+
+
 class Browser:
-    def __init__(self) -> None:
-        WIDTH, HEIGHT = 800, 600
+    def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
-            self.window, 
-            width=WIDTH, 
+            self.window,
+            width=WIDTH,
             height=HEIGHT
-            )
+        )
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+
 
     def load(self, url):
         body = url.request()
-        show(body)
-        self.canvas.create_rectangle(10,20,400,300)
-        self.canvas.create_oval(100,100,150,150)
-        self.canvas.create_text(200,150, text="Hi!")
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
 
+    def draw(self):
+        self.canvas.delete("all")
+        for x,y,c in self.display_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x,y-self.scroll,text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
     
-def show(body):
-    in_tag = False
-    for c in body: 
-        if c== "<":
-            in_tag = True
-        elif c == ">": 
-            in_tag = False 
-        elif not in_tag:
-            print(c, end="")
-
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.draw()
 
 
 if __name__ == "__main__":
-        import sys
-        Browser().load(URL(sys.argv[1]))
-        tkinter.mainloop()
-
-
-
-
-
+    import sys
+    Browser().load(URL("https://browser.engineering/examples/xiyouji.html"))
+    tkinter.mainloop()
